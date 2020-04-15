0fef99cf194cee926297e235973100ab6a789de5
Imron Hajiahmad <imronha@gmail.com>
2020-04-14 23:54:21 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+// Import components
+import { Cards, Chart, CountryPicker } from "./components";
+
+import styles from "./App.module.css";
+import { fetchData } from "./api";
+
+class App extends React.Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+  render() {
+    return (
+      <div className={styles.container}>
+        <Cards />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
+}
+
+export default App;
